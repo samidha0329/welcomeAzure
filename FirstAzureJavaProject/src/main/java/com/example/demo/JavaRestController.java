857c09f8a22214658abe543c63b5754de94dd3ef
Samidha Ratnaparkhi <SRatnaparkhi@slb.com>
2021-07-16 16:40:04 +0530

    Initial commit

---
+++ FirstAzureJavaProject/src/main/java/com/example/demo/JavaRestController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JavaRestController {
+	
+	@GetMapping("/welcome")
+	public String getWelcomeMessage() {
+		return "Welcome to Azure Java app";
+	}
+	
+	@GetMapping("/")
+	public String getMessage() {
+		return "Welcome to Azure app";
+	}
+	
+
+}
